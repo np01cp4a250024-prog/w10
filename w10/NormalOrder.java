c86116c8f132fc4f9b5746bac1dabea10bce9d70
Aryan <np01cp4a250024@islingtoncollege.edu.np>
2026-01-07 13:23:37 +0545

    hxa

---
+++ w10/NormalOrder.java
@@ -0,0 +1,25 @@
+package w10;
+
+
+/**
+ * Write a description of class NormalOrder here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NormalOrder extends Order {
+
+    NormalOrder(int orderId, String customerName, double amount) {
+        super(orderId, customerName, amount);
+    }
+
+    @Override
+    public double calculateFinalAmount() {
+        return super.calculateFinalAmount() + 50;
+    }
+
+    public double calculateFinalAmount(double discountAmount) {
+        return calculateFinalAmount() - discountAmount;
+    }
+}
+
